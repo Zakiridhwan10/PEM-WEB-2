f24c317b0d557774775c9cda4888b95ecfc9364e
Zakiridhwan10 <92352052+Zakiridhwan10@users.noreply.github.com>
2022-02-26 15:00:25 +0700

    Add files via upload

---
+++ Pertemuan 1/arr01.php
@@ -0,0 +1,27 @@
+<?php
+
+// //
+// $mahasiswa = ['Sabiq', 'Hanafi', 'Dea'];
+
+// echo $mahasiswa[0];
+// echo "<br/>";
+// echo $mahasiswa[1];
+// echo "<br/>";
+// echo $mahasiswa[2];
+// echo "<br/>";
+
+
+// //
+// foreach ($mahasiswa as $mhs) {
+//     echo $mhs;
+//     echo "<br/>";
+// }
+
+$mahasiswa = [
+    ["nama" => "Sabiq", "NIM" => "01102212133", "UTS" => 100, "UAS" => 100],
+    ["nama" => "Dea", "NIM" => "01102212133", "UTS" => 100, "UAS" => 100],
+];
+
+echo $mahasiswa[0]["nama"];
+?>
+
---
+++ Pertemuan 1/nyoba.php
@@ -0,0 +1,19 @@
+<?php
+
+$nama = 'Sabiq';
+$umur = 20;
+$berat = 52;
+
+echo "Nama saya $nama umur $umur tahun berat $berat Kg";
+echo "<br/>"; 
+
+//Variable Sistem
+echo 'Dokumen Root ' .$_SERVER["DOCUMENT_ROOT"];
+echo "<br/>";
+
+//Variable Konstan
+define('makanan', 'Sushi');
+echo makanan;
+
+//
+?>
